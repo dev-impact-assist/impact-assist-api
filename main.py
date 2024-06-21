46f13286c8ca8857b00d10f188aec722bd198a60
Matt Ravenmoore <m.ravenmoore@gmail.com>
2024-06-21 14:48:08 -0700

    test route established

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"details":f"Hello,My special number is = {env['SPECIAL_NUM']}"}
+
+@app.get('/test')
+def test_page():
+    return {"content":"This is the test end point"}
